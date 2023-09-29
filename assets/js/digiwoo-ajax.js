a345bdb16932a7fc8eaae7c7a86b7baa153e0e8f
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-09-29 11:52:22 +0800

    Update digiwoo-ajax.js

--- assets/js/digiwoo-ajax.js
+++ assets/js/digiwoo-ajax.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
     $('#digiwoo_category_select').on('change', function() {
         var category_id = $(this).val();
         console.log(digiwoo_params.ajax_url);
-
+        console.log(category_id);
         $.ajax({
             url: digiwoo_params.ajax_url,
             type: 'POST',
